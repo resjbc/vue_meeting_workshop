b6b5a4924304cdc150e42cfaca5a1484c1db5d3d
ResHitman <resjbc@hotmail.com>
2018-10-22 15:15:55 +0700

    ep2-07

--- Frontend/src/main.js
+++ Frontend/src/main.js
@@ -3,7 +3,12 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap/dist/js/bootstrap'
+import * as jquery from 'jquery'
+
 Vue.config.productionTip = false
+Vue.prototype.jquery = jquery;
 
 new Vue({
   router,
